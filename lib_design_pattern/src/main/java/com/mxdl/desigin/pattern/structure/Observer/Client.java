f0d99dfaed921f822de05c2807b01fee661d9442
geduo83 <geduo_83@163.com>
2019-12-20 17:59:45 +0800

    code perfect

--- lib_design_pattern/src/main/java/com/mxdl/desigin/pattern/structure/Observer/Client.java
+++ lib_design_pattern/src/main/java/com/mxdl/desigin/pattern/structure/Observer/Client.java
@@ -1,20 +1,19 @@
 package com.mxdl.desigin.pattern.structure.Observer;
 
 /**
- * Description: <Test><br>
+ * Description: <Client><br>
  * Author:      mxdl<br>
  * Date:        2019/12/19<br>
  * Version:     V1.0.0<br>
  * Update:     <br>
  */
-public class Test {
+public class Client {
     public static void main(String[] args) {
         Observer1 observer1 = new Observer1();
         Observer2 observer2 = new Observer2();
-        MyObserverable observerable = new MyObserverable();
+        ConcreateObserverable observerable = new ConcreateObserverable();
         observerable.addObserver(observer1);
         observerable.addObserver(observer2);
         observerable.update();
-        System.out.println("ok");
     }
 }
--- lib_design_pattern/src/main/java/com/mxdl/desigin/pattern/structure/Observer/ConcreateObserverable.java
+++ lib_design_pattern/src/main/java/com/mxdl/desigin/pattern/structure/Observer/ConcreateObserverable.java
@@ -3,13 +3,13 @@ package com.mxdl.desigin.pattern.structure.Observer;
 import java.util.Observable;
 
 /**
- * Description: <MyObserverable><br>
+ * Description: <ConcreateObserverable><br>
  * Author:      mxdl<br>
  * Date:        2019/12/19<br>
  * Version:     V1.0.0<br>
  * Update:     <br>
  */
-public class MyObserverable extends Observable {
+public class ConcreateObserverable extends Observable {
     void update(){
         setChanged();
         notifyObservers();
